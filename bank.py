fc323d5b672d23a328ec288596b00b705d0bb2f1
michaelmurwayi <mmurwayi@ona.io>
2019-07-18 15:40:50 +0300

    creating multiple accounts

--- bank.py
+++ bank.py
@@ -1,38 +1,3 @@
-import csv
-
-
-def create_accounts(csvfile):
-    accounts= {
-            "first_name": str,
-            "last_name": str,
-            "account_name": str,
-            "account_number": str,
-            "account_type": str,
-            "branch_name": str,
-            "phonenumber": str
-
-        } 
-    
-    with open(csvfile, "r") as data:
-        reader = csv.reader(data)
-        for row in reader:
-            accounts["first_name"] = row[0]
-            accounts["last_name"]= row[1]
-            accounts["account_name"]= row[2]
-            accounts["account_number"]= row[3]
-            accounts["account_type"]= row[4]
-            accounts["branch_name"]= row[5]
-            accounts["phonenumber"]= row[6]
-            print(accounts)
-    return accounts
-
-def get_row(file):
-    with open("accounts.csv", "r") as data:
-        reader = csv.reader(data)
-        for item in reader:
-            row = item
-    return row  
-                    
 def create_account(row):
     account = {
             "first_name":row[0] ,
@@ -43,14 +8,12 @@ def create_account(row):
             "branch_name": row[5],
             "phonenumber": row[6],
         }
-       
+    # print(account)
     return account
+def create_accounts(rows):
+    accounts = []
+    for items in rows:
+        accounts.append(create_account(items))
+    return accounts
 
-def main():
-    create_accounts("accounts.csv")
-    get_row("accounts.csv")
-
-
-
-if __name__ == "__main__":
-    main()
+       
--- tests.py
+++ tests.py
@@ -12,24 +12,26 @@ expected_value_for_account = {
     "phonenumber": " +254746256084",
 }
 
-expected_value_for_accounts = {
-    "first_name": "Mark",
-    "last_name": "Anderson",
-    "account_name": "MarkAnderson",
-    "account_number": "421",
-    "account_type": "fixed",
-    "branch_name": "kajiado",
-    "phonenumber": "+254790134102",
-}
-{
-    "first_name": "Michael ",
-    "last_name": " Murwayi",
-    "account_name": "MichaelMurwayi",
-    "account_number": "420",
-    "account_type": "saving",
-    "branch_name": "Thika",
-    "phonenumber": " +254746256084",
-}
+expected_value_for_accounts = [
+    {
+        "first_name": "Michael ",
+        "last_name": " Murwayi",
+        "account_name": "MichaelMurwayi",
+        "account_number": "420",
+        "account_type": "saving",
+        "branch_name": "Thika",
+        "phonenumber": " +254746256084",
+    },
+    {
+        "first_name": "Mark ",
+        "last_name": " Anderson",
+        "account_name": "MarkAnderson",
+        "account_number": "421",
+        "account_type": "fixed",
+        "branch_name": "kajiado",
+        "phonenumber": "+254790134102",
+    },
+]
 
 
 class TestBank(unittest.TestCase):
@@ -47,8 +49,27 @@ class TestBank(unittest.TestCase):
         self.assertEqual(account, expected_value_for_account)
 
     def test_create_accounts(self):
-        file_name = "accounts.csv"
-        self.assertEqual(create_accounts(file_name), expected_value_for_accounts)
+        rows = [
+            [
+                "Michael ",
+                " Murwayi",
+                "MichaelMurwayi",
+                "420",
+                "saving",
+                "Thika",
+                " +254746256084",
+            ],
+            [
+                "Mark ",
+                " Anderson",
+                "MarkAnderson",
+                "421",
+                "fixed",
+                "kajiado",
+                "+254790134102",
+            ],
+        ]
+        self.assertEqual(create_accounts(rows), expected_value_for_accounts)
 
 
 if __name__ == "__main__":
