1f82fd019db5b9d027182f40dcfe6951a65fb173
michaelmurwayi <mmurwayi@ona.io>
2019-07-18 12:48:55 +0300

    create single account

--- bank.py
+++ bank.py
@@ -2,7 +2,6 @@ import csv
 
 
 def create_accounts(csvfile):
-    global accounts 
     accounts= {
             "first_name": str,
             "last_name": str,
@@ -25,51 +24,32 @@ def create_accounts(csvfile):
             accounts["branch_name"]= row[5]
             accounts["phonenumber"]= row[6]
             print(accounts)
-            return accounts
+    return accounts
 
 def get_row(file):
     with open("accounts.csv", "r") as data:
         reader = csv.reader(data)
         for item in reader:
-            global row
             row = item
-            return row  
-            
-        
-
-
+    return row  
+                    
 def create_account(row):
     account = {
-            "first_name":str ,
-            "last_name": str,
-            "account_name": str,
-            "account_number": str,
-            "account_type": str ,
-            "branch_name": str,
-            "phonenumber": str,
+            "first_name":row[0] ,
+            "last_name": row[1],
+            "account_name": row[2],
+            "account_number": row[3],
+            "account_type": row[4],
+            "branch_name": row[5],
+            "phonenumber": row[6],
         }
-    for key, value in account.items():
-        if key ==  "first_name":
-            account["first_name"] = row[0]
-        elif key == "last_name":
-            account["last_name"] = row[1]
-        elif key == "account_name":
-            account["account_name"] = row[2]
-        elif key == "account_number":
-            account["account_number"] = row[3]
-        elif key == "account_type":
-            account["account_type"] = row[4]
-        elif key == "branch_name":
-            account["branch_name"] = row[5]
-        elif key == "phonenumber":
-            account["phonenumber"] = row[6]
-            print(account)    
-            return account
+       
+    return account
 
 def main():
     create_accounts("accounts.csv")
     get_row("accounts.csv")
-    create_account(row)
+
 
 
 if __name__ == "__main__":
