e16e4143edf6c636fe56b1381aaac15ef561d0d9
michaelmurwayi <mmurwayi@ona.io>
2019-07-22 19:47:41 +0300

    validation of account inputs

--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
 import unittest
+import logging
+from testfixtures import LogCapture
 from bank import create_account, create_accounts, create_accounts_from_csv
 
-unittest.TestCase.maxDiff = None
+maxDiff = None
 
 expected_value_for_account = {
     "first_name": "Michael ",
@@ -78,6 +80,71 @@ class TestBank(unittest.TestCase):
         self.assertEqual(
             create_accounts_from_csv(csv_file), expected_value_for_accounts
         )
+
+    def test_validation_of_firstname(self):
+        row = ["", "Murwayi", "MichaelMurwayi", 420, "saving", "Thika", "+254746256084"]
+        expected_value = (
+            "Missing frist name on record",
+            ["", "Murwayi", "MichaelMurwayi", 420, "saving", "Thika", "+254746256084"],
+        )
+        self.assertEqual(create_account(row), expected_value)
+
+    def test_validation_of_lastname(self):
+        row = ["Michael", "", "MichaelMurwayi", 420, "saving", "Thika", "+254746256084"]
+        expected_value = (
+            "Missing second name on the record ",
+            ["Michael", "", "MichaelMurwayi", 420, "saving", "Thika", "+254746256084"],
+        )
+        self.assertEqual(create_account(row), expected_value)
+
+    def test_validation_of_account_type(self):
+        row = [
+            "Michael",
+            "Murwayi",
+            "MichaelMurwayi",
+            "420",
+            "saving",
+            "Thika",
+            "+254746256084",
+        ]
+        expected_value = (
+            "Account number must be a Number",
+            [
+                "Michael",
+                "Murwayi",
+                "MichaelMurwayi",
+                "420",
+                "saving",
+                "Thika",
+                "+254746256084",
+            ],
+        )
+        self.assertEqual(create_account(row), expected_value)
+
+    def test_validation_of_account_type(self):
+        row = [
+            "Michael",
+            "Murwayi",
+            "MichaelMurwayi",
+            420,
+            "save",
+            "Thika",
+            "+254746256084",
+        ]
+        expected_value = (
+            "provide valid account type",
+            [
+                "Michael",
+                "Murwayi",
+                "MichaelMurwayi",
+                420,
+                "save",
+                "Thika",
+                "+254746256084",
+            ],
+        )
+
+        self.assertEqual(create_account(row), expected_value)
 
 
 if __name__ == "__main__":
