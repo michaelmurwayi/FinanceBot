464993c77dc014448f05ed6fc179d0cc79b511b6
michaelmurwayi <mmurwayi@ona.io>
2019-07-29 16:13:16 +0300

    modification for models in accounts

--- bank/Accounts/models.py
+++ bank/Accounts/models.py
@@ -5,7 +5,7 @@ class Users(models.Model):
     first_name = models.CharField(max_length=50)
     second_name = models.CharField(max_length=50)
     account_name = models.CharField(max_length=100)
-    account_number = models.IntegerField(primary_key=True)
+    account_number = models.IntegerField(primary_key=True , unique=True)
     account_type = models.CharField(max_length=10)
     branch_name = models.CharField(max_length=20)
     phonenumber = models.CharField(max_length=20)
