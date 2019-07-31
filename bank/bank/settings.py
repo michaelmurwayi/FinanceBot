eeeab882a060a2933847c847737d2146ad661b4f
michaelmurwayi <mmurwayi@ona.io>
2019-07-31 14:43:51 +0300

    popukating the mysql db

--- bank/bank/settings.py
+++ bank/bank/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Accounts',
-    'Transactions'
+    'Transactions',
 ]
 
 MIDDLEWARE = [
@@ -78,7 +79,9 @@ WSGI_APPLICATION = 'bank.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'banks',
+        'HOST': 'localhost',
+        'NAME': 'bank',
+        'USER': 'root',
         'password': '',
     }
 }
