dac604e602a1c32d6a640772d6d62d00bc52943a
michaelmurwayi <mikemurwayi7@gmail.com>
2019-09-12 09:35:41 +0300

    Validation for data recieved from file and test for validation

--- bank/Transactions/test.py
+++ bank/Transactions/test.py
@@ -34,7 +34,7 @@ expected_value_for_records = [
 
 
 class TestRecordTransactions(unittest.TestCase):
-    maxDiff = None
+    # maxDiff = None
 
     def test_create_record(self):
         """ create record from single row"""
@@ -82,6 +82,144 @@ class TestRecordTransactions(unittest.TestCase):
         self.assertEqual(
             get_records_from_csv("transaction.csv"), expected_value_for_records
         )
+
+    def test_vadation_for_blank_accountname(self):
+        """ validate that account name is not blank"""
+
+        row = ["", "fixed", "Thika", "0745454545", "withdraw", "420", "2000.00"]
+        expected_value = (
+            "Account Name cannot be  blank",
+            {
+                "accountname": "",
+                "accounttype": "fixed",
+                "branchname": "Thika",
+                "phonenumber": "0745454545",
+                "transactiontype": "withdraw",
+                "accountnumber": 420,
+                "transactionamount": 2000.00,
+            },
+        )
+
+        self.assertEqual(create_record(row), expected_value)
+
+    def test_vadation_for_blank_accounttype(self):
+        """Validate account type is not blank """
+
+        row = [
+            "MichaelMurwayi",
+            "",
+            "Thika",
+            "0745454545",
+            "withdraw",
+            "420",
+            "2000.00",
+        ]
+        expected_value = (
+            "Account Type cannot be  blank",
+            {
+                "accountname": "MichaelMurwayi",
+                "accounttype": "",
+                "branchname": "Thika",
+                "phonenumber": "0745454545",
+                "transactiontype": "withdraw",
+                "accountnumber": 420,
+                "transactionamount": 2000.00,
+            },
+        )
+
+        self.assertEqual(create_record(row), expected_value)
+
+    def test_vadation_for_blank_branchname(self):
+        """Validate account type is not blank """
+
+        row = [
+            "MichaelMurwayi",
+            "fixed",
+            "",
+            "0745454545",
+            "withdraw",
+            "420",
+            "2000.00",
+        ]
+        expected_value = (
+            "branchname cannot be  blank",
+            {
+                "accountname": "MichaelMurwayi",
+                "accounttype": "fixed",
+                "branchname": "",
+                "phonenumber": "0745454545",
+                "transactiontype": "withdraw",
+                "accountnumber": 420,
+                "transactionamount": 2000.00,
+            },
+        )
+        # import ipdb;ipdb.set_trace()
+        self.assertEqual(create_record(row), expected_value)
+
+    def test_vadation_for_blank_phonenumber(self):
+        """Validate account type is not blank """
+
+        row = ["MichaelMurwayi", "fixed", "Thika", "", "withdraw", "420", "2000.00"]
+        expected_value = (
+            "Phonenumber cannot be  blank",
+            {
+                "accountname": "MichaelMurwayi",
+                "accounttype": "fixed",
+                "branchname": "Thika",
+                "phonenumber": "",
+                "transactiontype": "withdraw",
+                "accountnumber": 420,
+                "transactionamount": 2000.0,
+            },
+        )
+
+        self.assertEqual(create_record(row), expected_value)
+
+    def test_vadation_for_blank_transactiontype(self):
+        """Validate account type is not blank """
+
+        row = ["MichaelMurwayi", "fixed", "Thika", "0745454545", "", "420", "2000.00"]
+        expected_value = (
+            "Transaction Type cannot be  blank",
+            {
+                "accountname": "MichaelMurwayi",
+                "accounttype": "fixed",
+                "branchname": "Thika",
+                "phonenumber": "0745454545",
+                "transactiontype": "",
+                "accountnumber": 420,
+                "transactionamount": 2000.00,
+            },
+        )
+
+        self.assertEqual(create_record(row), expected_value)
+
+    def test_vadation_for_blank_accountnumber(self):
+        """Validate account type is not blank """
+
+        row = [
+            "MichaelMurwayi",
+            "",
+            "Thika",
+            "0745454545",
+            "withdraw",
+            "420",
+            "2000.00",
+        ]
+        expected_value = (
+            "Account Type cannot be  blank",
+            {
+                "accountname": "MichaelMurwayi",
+                "accounttype": "",
+                "branchname": "Thika",
+                "phonenumber": "0745454545",
+                "transactiontype": "withdraw",
+                "accountnumber": 420,
+                "transactionamount": 2000.00,
+            },
+        )
+
+        self.assertEqual(create_record(row), expected_value)
 
 
 if __name__ == "__main__":
